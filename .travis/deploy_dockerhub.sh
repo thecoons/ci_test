823798899988867fab0807eb1851d1d285f04b15
Antonin Barthelemy <antobarth@gmail.com>
2018-08-12 13:53:28 +0200

    Add configuration for Heroku staging

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 echo '### Heroku registry connection'
 echo "$DOCKER_PASSWORD" | docker login --username=$DOCKER_USER --password-stdin
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-echo '### repo:tag ==> '$TRAVIS_REPO_SLUG:$TAG
+echo '### Latest Tag : Repository:LatestTag ==> '$TRAVIS_REPO_SLUG:latest
 echo '### Build image'
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:latest .
 echo '### Push image to DockerHub'
 docker push $TRAVIS_REPO_SLUG
+echo '###Release Repository:PullRequestNumber ===> '$TRAVIS_REPO_SLUG:$TRAVIS_COMMIT
+docker tag $TRAVIS_REPO_SLUG:latest $TRAVIS_REPO_SLUG:$TRAVIS_COMMIT
+echo '### Push release image to DockerHub'
+docker push $TRAVIS_REPO_SLUG:$TRAVIS_COMMIT
 echo '### Logout registry'
 docker logout
